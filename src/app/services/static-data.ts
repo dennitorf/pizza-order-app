f832b2102cae69c6bc821e0d38163ad787ef85eb
henrry <henamorado29102@gmail.com>
2024-08-07 12:38:21 -0400

    Creating visual to select pizza size and toppings

--- src/app/services/static-data.ts
+++ src/app/services/static-data.ts
@@ -5,14 +5,13 @@ export const SIZE_DATA = [
     { id : 'extra-large', description : 'Extra Large', price : 9 }
   ]
 
-export const  TOPPINGS = [
-  { id : "tom", description : "Tomates", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Onios", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Bell pepper", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Mushrooms", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Tomates", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Pineapple", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Sausage", doubleCountForOffers : false, toppingTypeId : "veg" },
-  { id : "tom", description : "Pepperone", doubleCountForOffers : true, toppingTypeId : "veg" },
-  { id : "tom", description : "Barbecue chicken", doubleCountForOffers : true, toppingTypeId : "veg" },
-]
+  export const  TOPPINGS = [
+    { id: "tom", description: "Tomates", doubleCountForOffers: false, toppingTypeId: "veg", price: 1 },
+    { id: "oni", description: "Onions", doubleCountForOffers: false, toppingTypeId: "veg", price: 0.5 },
+    { id: "bpe", description: "Bell pepper", doubleCountForOffers: false, toppingTypeId: "veg", price: 1 },
+    { id: "msh", description: "Mushrooms", doubleCountForOffers: false, toppingTypeId: "veg", price: 1.2 },
+    { id: "pin", description: "Pineapple", doubleCountForOffers: false, toppingTypeId: "veg", price: 0.75 },
+    { id: "sau", description: "Sausage", doubleCountForOffers: false, toppingTypeId: "non-veg", price: 1 },
+    { id: "pep", description: "Pepperoni", doubleCountForOffers: true, toppingTypeId: "non-veg", price: 2 },
+    { id: "bbq", description: "Barbecue chicken", doubleCountForOffers: true, toppingTypeId: "non-veg", price: 3 }
+  ]
